233b3c6fe7778cbefdf74c55387757623ccf8551
MEFISTOTIL <s.e.m.e.n.o.v.k.v.a.v@gmail.com>
2022-07-14 14:06:17 +0300

    good

--- src/index.js
+++ src/index.js
@@ -1,3 +1,84 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let openBracets = [];
+	let closeBracets = [];
+	let dubleBracets = [];
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		if (bracketsConfig[i][0] === bracketsConfig[i][1]) {
+			dubleBracets.push(bracketsConfig[i][0])
+		} else {
+			openBracets.push(bracketsConfig[i][0]);
+			closeBracets.push(bracketsConfig[i][1]);
+		}
+	}
+
+	let stack = [];
+	for (let i = 0; i < str.length; i++) {
+		let curentSymbvol = str[i];
+		// Запушиваем в стак если элемент находиться в массиве открытых скобок
+		if (openBracets.includes(curentSymbvol)) {
+			stack.push(curentSymbvol)
+		}
+		// Убираме со стака если элемент находиться в массиве зкрытых скобок равную по индексу...
+		else if (closeBracets.includes(curentSymbvol)) {
+			if (stack === 0) {
+				return false;
+			}
+			let topElement = stack[stack.length - 1];
+			if (closeBracets.indexOf(curentSymbvol) == openBracets.indexOf(topElement)) {
+				stack.pop()
+			}
+			else {
+				return false;
+			}
+		}
+		// Если элемент находиться в массиве, где закрытие и открытие одинаковы
+		else if (dubleBracets.includes(curentSymbvol)) {
+			let te = stack[stack.length - 1];
+			if (te == curentSymbvol) {
+				stack.pop();
+			} else {
+				stack.push(curentSymbvol)
+			}
+		}
+	}
+	return stack.length === 0;
 }
+
+// const config1 = [['(', ')']];
+// const config2 = [['(', ')'], ['[', ']']];
+// const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+// const config4 = [['|', '|']];
+// const config5 = [['(', ')'], ['|', '|']];
+// const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+// const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+
+// function check(str, bracketsConfig) {
+	
+// }
+
+// console.log(check('||', config4))
+
+// function check(str, bracketsConfig) {
+
+
+// 	let score = 0;
+// 	for (let i = 0; i < str.length; i++) {
+// 		if (openBracets.includes(str[i])) {
+// 			score++;
+// 		}
+// 		else {
+// 			score--;
+// 		}
+// 		if (score < 0) {
+// 			return false;
+// 		}
+// 	}
+// 	if (score > 0) {
+// 		return false;
+// 	}
+// 	else {
+// 		return true;
+// 	}
+// }
+
+// console.log(check(')(', config7))
